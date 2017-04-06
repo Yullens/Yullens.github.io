b912ea3914bcc3db1e4cb2a9248f016955586a0a
Yullens <jerentgary@gmail.com>
2017-04-06 20:48:39 +0200

    Data

---
+++ bitcoin/Data.js
@@ -0,0 +1,18 @@
+<script src="https://www.gstatic.com/firebasejs/3.7.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.7.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.7.1/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.7.1/firebase-messaging.js"></script>
+
+
+<script src="https://www.gstatic.com/firebasejs/3.7.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAORNTKlXoQnGLFZGiyuAuk0JnzA8ZSIHY",
+    authDomain: "ping-93ac3.firebaseapp.com",
+    databaseURL: "https://ping-93ac3.firebaseio.com",
+    storageBucket: "ping-93ac3.appspot.com",
+    messagingSenderId: "188392952544"
+  };
+  firebase.initializeApp(config);
+</script>
